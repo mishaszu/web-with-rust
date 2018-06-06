26a242fa5841cb8163e98bbc7166b6d7c7b4689e
Misha Szulczewski <mishaszu@gmail.com>
2018-06-06 15:52:06 +0200

    website configuration

---
+++ web/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => <h1>hello world</h1>
+
+ReactDom.render(<App />,document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './web/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webassembly intro',
+            inject: 'body',
+            template: './web/index.html',
+        })
+    ],
+    devServer: {
+        contentBase: 'build',
+        port: 8000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node-modules/,
+                use: ['babel-loader'],
+            }
+        ]
+    }
+}
